7c96d45e32fa41148e02d5346c0c0dbdc1fb12f6
Sean Fallmann <jorundr@hotmail.com>
2015-10-07 22:15:30 -0400

    Testing\messing around to learn how things work with the Google Maps Api

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 
+(function(global) {
+
+
+
+        global.initMap = function() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {
+                lat: 36.098344,
+                lng: -80.015482
+            },
+            zoom: 12
+
+
+        });
+
+        var marker = new google.maps.Marker({
+            position: {
+                lat: 36.073982,
+                lng: -79.950307
+            },
+            map: map,
+            title: 'Progressive Business Media'
+
+        });
+
+    }
+}(window));
+
