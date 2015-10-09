99bf5026846e14d394691fc7350419e2fe376298
sfallmann <sfallmann@pbmbrands.com>
2015-10-09 15:54:16 -0400

    cleaning up code; trying to decouple and separate concerns as much as possible

--- js/app.js
+++ js/app.js
@@ -15,7 +15,10 @@
         };
 
         // creates map object
+        panorama = new google.maps.StreetViewPanorama(document.getElementById('street-view'));
         map = new google.maps.Map(document.getElementById('map'), mapData);
+
+
 
         // starts rest of app once the map is created
         start();
@@ -27,12 +30,13 @@
 
 
 
+
         // the starting marker data - the Model
         // TODO: Pass coords through StreetViewService which will return approx coords that have StreetViews assoicated with them
         var markerDataArr = [
 
             {
-                title: "Regal Cinems - Palladium",
+                title: "Regal Cinemas - Palladium",
                 position: {
                     lat: 36.0347243,
                     lng: -79.9616307
@@ -54,28 +58,101 @@
             }
         ];
 
-        var controller = function(){
+
+        function Controller(data){
+
+            this.markersData = data;
+            this.markersVM;
+
+            this.sv = new google.maps.StreetViewService();
+
+
+        }
+
+        Controller.prototype = {
+
+            initMapListeners: function(){
+                var self = this;
+                map.addListener('click', function(e) {
+                    var marker = self.addMarker(e.latLng, map);
+
+                });
+            },
+
+            initMarkers: function(){
+
+                var self = this;
+                var markers = [];
+
+
+                self.markersData.forEach(function(item){
+                    item.map = map;
+
+                    var mapMarker = new google.maps.Marker(item);
+
+                    console.log(self.sv.getPanorama({ location: item.position, radius: 500 }, processSVData));
+
+
+
+
+                    var marker = new Marker(item.title, mapMarker, panorama);
+                    markers.push(ko.observable(marker));
+                });
+
+                return markers;
+
+            },
+
+            init: function(){
+                this.initMapListeners();
+                this.markersVM = new MarkersViewModel(this.initMarkers());
+                ko.applyBindings(this.markersVM);
+                console.log(this.markersVM);
+            },
+
+            addMarker: function(latLng, map) {
+
+                var mapMarker = new google.maps.Marker({
+                    position: latLng,
+                    map: map
+                });
+
+                var panorama = new google.maps.StreetViewPanorama(
+                    document.getElementById('street-view'),
+                    {
+                        position: latLng,
+                        pov: {heading: 165, pitch: 0},
+                        zoom: 1,
+                        visible: false
+                    }
+                );
+
+                var marker = new Marker("No Title", mapMarker, panorama);
+                this.markersVM.markers.push(ko.observable(marker));
+
+                map.panTo(latLng);
+
+            }
 
 
 
         }
 
-        function MarkersViewModel(initData){
+        function MarkersViewModel(markers){
+
             var self = this;
 
-            this.initData = initData;
+            this.markers = ko.observableArray(markers);
 
-            self.markers = ko.observableArray();
+            this.inputSelected = ko.observable(false);
+            this.editingMarker = false;
+            this.clickedMarker;
+            this.clickedElement;
 
 
-
-
-            self.inputSelected = ko.observable(false);
-            self.editingMarker = false;
-            self.clickedMarker;
-            self.clickedElement;
-
-            self.editTitle = function(marker){
+            this.editTitle = function(marker){
+                console.log("Editing Title");
+                console.log(self);
                 if (!self.inputSelected() && !self.editingMarker){
                     marker.readyToEdit(false);
                     self.inputSelected(true);
@@ -83,7 +160,7 @@
                 }
             };
 
-            self.doneEditing = function(marker, event){
+            this.doneEditing = function(marker, event){
                 var title = marker.title();
 
                 if(event.keyCode == 13){
@@ -98,7 +175,7 @@
                 }
             };
 
-            self.showPanorama = function(marker, event){
+            this.showPanorama = function(marker, event){
 
                 $("#street-view").removeClass("hide");
 
@@ -116,77 +193,48 @@
                     self.clickedMarker = marker;
                 }
 
-            };
-
+            }
         }
 
 
-        MarkersViewModel.prototype = {
+        function processSVData(data, status) {
 
-            init: function(){
 
-                var self = this;
-                this.sv = new google.maps.StreetViewService();
+            var marker;
 
-                self.initData.forEach(function(item){
-                    item.map = map;
-                    console.log(self.sv);
-                    var mapMarker = new google.maps.Marker(item);
+            if (status === google.maps.StreetViewStatus.OK) {
 
-                    self.sv.getPanorama({ location: markerData.position }, processSVData);
-
-                    var panorama = new google.maps.StreetViewPanorama(
-                        document.getElementById('street-view'),
-                        {
-                            position: item.position   ,
-                            pov: {heading: 165, pitch: 0},
-                            zoom: 1,
-                            visible: false
-                        }
-                    );
-
-                    var marker = new Marker(item.title, mapMarker, panorama);
-                    self.markers.push(ko.observable(marker));
+                marker = new google.maps.Marker({
+                    position: data.location.latLng,
+                    map: map,
+                    title: data.location.description
                 });
 
+                panorama.setPano(data.location.pano);
+                panorama.setPov({
+                    heading: 270,
+                    pitch: 0
+                });
+                panorama.setVisible(true);
+
+                marker.addListener('click', function() {
+                    var markerPanoID = data.location.pano;
+                    // Set the Pano to use the passed panoID.
+                    panorama.setPano(markerPanoID);
+                    panorama.setPov({
+                        heading: 270,
+                        pitch: 0
+                    });
+                    panorama.setVisible(true);
+                });
+            } else {
+                console.error('Street View data not found for this location.');
             }
 
-        }
+            console.log(marker, panorama);
+            var mp =  {marker: marker, panorama: panorama};
+            return mp;
 
-
-        map.addListener('click', function(e) {
-            placeMarkerAndPanTo(e.latLng, map);
-        });
-
-
-
-
-
-        function processSVData(data, status){
-            console.log(data, status);
-        }
-
-        function placeMarkerAndPanTo(latLng, map) {
-
-            var mapMarker = new google.maps.Marker({
-                position: latLng,
-                map: map
-            });
-
-            var panorama = new google.maps.StreetViewPanorama(
-                document.getElementById('street-view'),
-                {
-                    position: latLng,
-                    pov: {heading: 165, pitch: 0},
-                    zoom: 1,
-                    visible: false
-                }
-            );
-
-            var marker = new Marker("No Title", mapMarker, panorama);
-            markers.push(ko.observable(marker));
-
-            map.panTo(latLng);
         }
 
 
@@ -199,11 +247,9 @@
         };
 
 
+        var ctrl = new Controller(markerDataArr);
+        ctrl.init();
 
-
-        var markersViewModel = new MarkersViewModel();
-
-        ko.applyBindings(markersViewModel);
 
     }
 
